722d50823aff4f699577e5c9e82a715e0656077d
Kayley Chery <kac7748@nyu.edu>
2025-06-26 12:09:55 -0400

    Initial Commit

---
+++ GitHubCollaboration/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GitHubCollaboration
+//
+//  Created by Kayley Chery on 6/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
