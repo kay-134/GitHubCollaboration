976db0a998327fad986089e63c73568532de384a
Kayley Chery <kac7748@nyu.edu>
2025-06-26 12:34:36 -0400

    NEW CHANGES

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -17,6 +17,7 @@ struct ContentView: View {
             Text("Change 1")
             Text("Change 2")
             Text("Change 3")
+            Text("Change made by Kayley")
             List {
                 ForEach(items) { item in
                     NavigationLink {
